3b44eff7e3fa91942a94547babdd69f1ae31d67f
RezaBakhshiNia <Reza.bakhshi1381r@gmail.com>
2024-09-16 10:52:26 +0330

    setup middleware to redirect user based on its device

--- middleware.ts
+++ middleware.ts
@@ -1,16 +1,18 @@
 import { NextResponse, NextRequest, userAgent } from "next/server";
 
 export function middleware(request: NextRequest) {
+  // detect user device
   const { device } = userAgent(request);
 
   if (device.type === "mobile") {
+    // Redirect to mobile page
     return NextResponse.redirect(new URL("/mobile", request.url));
   } else {
+    // Redirect to mobile page
     return NextResponse.redirect(new URL("/desktop", request.url));
   }
-  // Redirect to login page if not authenticated
 }
 
 export const config = {
-  matcher: "/dashboard/:path*",
+  matcher: "/",
 };
