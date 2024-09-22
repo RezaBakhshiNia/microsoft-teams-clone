f5323a9b097eca0d0beb850d039040d2f8e448c2
RezaBakhshiNia <Reza.bakhshi1381r@gmail.com>
2024-09-22 10:21:01 +0330

    add arrow down to selected checkbox

--- components/UI/RoundedCheckBox.tsx
+++ components/UI/RoundedCheckBox.tsx
@@ -23,8 +23,7 @@ const CircleCheckbox = ({
   return (
     <label
       className={
-        "flex items-center gap-2 px-4 py-1 mx-1 rounded-[4px] " +
-        wrapperStyles
+        "flex items-center gap-2 px-4 py-1 mx-1 rounded-[4px] " + wrapperStyles
       }>
       <input
         type="checkbox"
@@ -34,7 +33,13 @@ const CircleCheckbox = ({
       />
       <div
         className={`circle-checkbox transition-colors duration-300 ease-in-out ${
-          checked ? index === 0 ? "bg-green-600" : "bg-indigo-500" : index === 0 ? "bg-indigo-500" : "bg-white"
+          checked
+            ? index === 0
+              ? "bg-green-600"
+              : "bg-indigo-500"
+            : index === 0
+              ? "bg-indigo-500"
+              : "bg-white"
         }`}
         style={{
           width: `${size}px`,
@@ -71,6 +76,19 @@ const CircleCheckbox = ({
         </svg>
       </div>
       <span className={titleStyles}>{title}</span>
+      {index === 0 ? (
+        <svg
+          width="8"
+          height="5"
+          viewBox="0 0 8 5"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg">
+          <path
+            d="M0.146447 0.646447C0.341709 0.451184 0.658291 0.451184 0.853553 0.646447L4 3.79289L7.14645 0.646447C7.34171 0.451184 7.65829 0.451184 7.85355 0.646447C8.04882 0.841709 8.04882 1.15829 7.85355 1.35355L4.35355 4.85355C4.15829 5.04882 3.84171 5.04882 3.64645 4.85355L0.146447 1.35355C-0.0488155 1.15829 -0.0488155 0.841709 0.146447 0.646447Z"
+            fill="white"
+          />
+        </svg>
+      ) : null}
     </label>
   );
 };
